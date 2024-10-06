6b862ad2337988a1a5c9d609b2e65c108fd5dcc6
Chinmay <Chinmayj745@gmail.com>
2024-10-06 22:01:27 +0530

    Admin base authentication is added in to the project

--- src/main/java/com/journals/journalApp/configuration/SpringConfig.java
+++ src/main/java/com/journals/journalApp/configuration/SpringConfig.java
@@ -28,6 +28,7 @@ public class SpringConfig {
        return  http.authorizeHttpRequests(request -> request
                         .requestMatchers("/public/**").permitAll()
                         .requestMatchers("/journal/**","/user/**").authenticated()
+                        .requestMatchers("/admin/**").hasRole("ADMIN")
                         .anyRequest().permitAll())
                 .httpBasic(Customizer.withDefaults())
                 .csrf(AbstractHttpConfigurer::disable)
